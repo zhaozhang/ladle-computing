8489d232fef77de2539479ea9dad1968773d63c7
yxiaohan <milanyxh@gmail.com>
2013-10-13 11:29:09 +0800

    yxh

--- trunk/yipiao/web/views/layouts/login.php
+++ trunk/yipiao/web/views/layouts/login.php
@@ -12,52 +12,24 @@
 <!--<script charset="utf-8" src="/static/bootstrap-2.3.1/js/bootstrap.min.js" charset="utf-8"></script>-->
 
 <!-- 引入EasyUI -->
-<link id="easyuiTheme" rel="stylesheet" href="/static/jquery-easyui/themes/metro-gray/easyui.css" type="text/css">
+<link id="easyuiTheme" rel="stylesheet" href="/static/jquery-easyui/themes/metro-blue/easyui.css" type="text/css">
 <link rel="stylesheet" href="/static/jquery-easyui/themes/icon.css" type="text/css">
 <link rel="stylesheet" type="text/css" href="/static/css/portal.css">
 
 <script type="text/javascript" src="/static/jquery-easyui/jquery.easyui.min.js" charset="utf-8"></script>
 <script type="text/javascript" src="/static/jquery-easyui/locale/easyui-lang-zh_CN.js" charset="utf-8"></script>
 
-<script type="text/javascript" src="/static/jMask.js" ></script>
-<script type="text/javascript" src="/static/cufon-yui.js" ></script>
-<script type="text/javascript" src="/static/GeosansLight_500.font.js"  ></script>
-<script type="text/javascript" src="/static/raphael-min.js"  ></script>
-<script type="text/javascript" src="/static/custom.js"  ></script>
+<link media="screen,projection" rel="stylesheet" type="text/css" href="/static/login/styles/screen.css" />
+<script type="text/javascript" src="/static/login/scripts/slides.min.jquery.js"></script>
+<script type="text/javascript" src="/static/login/scripts/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="/static/login/scripts/global.js"></script>
 
 <style type="text/css"> 
 <!-- 
 body { 
-background-image: url('/static/images/paper_1.png'); 
+background-image: url('/static/images/bg.jpg'); 
+background-size:cover;
 } 
-@charset "utf-8";
-/* CSS Document */
-.active { position:absolute;z-index:10; } /* Class for element at top  */
-.reset{ position:absolute;z-index:4;  } /* Class for the rest of the elements  */
-
-#jMask-stage ul { list-style:none; display:block; margin:0px; padding:0px; float:left; }
-#jMask-stage ul li { position:absolute; display:block; overflow:hidden;  }
-#jMask-stage ul img { position:absolute;  }
-
-h1 { font-size:36px; }
-
-#container { margin-left:30px;margin-top:70px;margin-right:auto;width:100px }
-
-#outerblock { position:relative; z-index:2; margin:50px; }
-
-#innerblock { position:absolute; z-index:999;  }
-#slideshow { padding:10px; }
-
-#innerblock #ribbon-left { background:url('/static/css/i/ribbonleft.png') no-repeat bottom left;  width:28px; height:72px; position:absolute;  margin-top:-160px; margin-left:-18px;}
-
-#innerblock .title { position:absolute; background:url('/static/css/i/ribbon-slice.png') repeat-x; border-right:1px solid #185381; border-top:1px solid #185381; border-bottom:1px solid #185381; padding:0px 4px 7px 0px; display:none; color:#fff;height:44px; margin-top:238px; z-index:3;
-}
-#innerblock p { position:absolute; width:630px; padding:10px 25px 10px 25px; text-align:justify; font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif; background:url('/static/css/i/desc-overlay.png');margin-top:300px;z-index:2; display:none; line-height: 20px; }
-
-#shadow { margin-top:-120px;margin-left:20px }
-
-#slideshow ul { list-style:none; }
-
 -->
 </style> 
 </head>
--- trunk/yipiao/web/views/site/login.php
+++ trunk/yipiao/web/views/site/login.php
@@ -1,60 +1,88 @@
 
 <div >
 
-	<div id="container" >
-	
-		<div id="outerblock">
-		<div id="innerblock">
-		
-		
-		<div id="slideshow">
-		<div>
-		<ul>
-		<li><img src="/static/images/im4.jpg" />
-		
-		<h1 class="title">Image Title 1</h1>
-		<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate neque id diam adipiscing ullamcorper. Nam sed metus et velit rhoncus hendrerit volutpat nec sapien. Nam ut velit laoreet est feugiat porta eu et dolor. 
-		
-		</p>
-		
-		</li>
-		<li><img src="/static/images/im3.jpg" />
-		<h1 class="title">Image Title 2</h1>
-		<p>
-		Lorem ipsum dolor sit amet, m adipiscing ullamcorper. Nam sed metus et velit rhoncus hendrerit volutpat nec sapien. Nam ut velit laoreet est feugiat porta eu et dolor. 
-		
-		</p>
-		
-		</li>
-		<li><img src="/static/images/im2.jpg" />
-		
-		<h1 class="title">Image Title 3</h1>
-		<p>
-		image 3  ,Lorem ipsum dolor sit amet, m adipiscing ullamcorper. Nam sed metus et velit rhoncus hendrerit volutpat nec sapien. Nam ut velit laoreet est feugiat porta eu et dolor. 
-		</p>
-		</li>
-		<li><img src="/static/images/im1.jpg" />
-		<h1 class="title">Image Title 4</h1>
-		<p>
-		image 4  ,Lorem ipsum dolor sit amet, m adipiscing ullamcorper. Nam sed metus et velit rhoncus hendrerit volutpat nec sapien. Nam ut velit laoreet est feugiat porta eu et dolor. 
-		</p>
-		</li>
-		
-		</ul>
-		</div>
-		</div>
-		<span id="ribbon-left"></span>
-		</div>
-		<div id="frame">
-		</div>
-
-		</div>
-		<img src="/static/css/i/shadow.png" class="grid_10" id="shadow" />
-	</div>
+	<br><br><br><br>
+	<div style="text-align:center;"><b><font face='微软雅黑' size='6'>高级中学成绩查询分析系统</font></b></div> 
+	<br>
+	<div id="content" class="wrap clearfix">
+		<div class="imgSliderContainer">
+		    <div class="imgSlider">
+				<div class="imgSliderC">
+					<div>
+						<img src="/static/login/contents/slider_image_1.jpg" alt="slider 1" />
+						<section>
+							<h2>成绩查询</h2>
+							<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							你可以在这里查询到每次考试各科的成绩以及班级和年级的统计</p>
+						</section>
+						<a href="#" class="imgSliderPrev imgSliderTextPrev"></a>
+						<a href="#" class="imgSliderNext imgSliderTextNext"></a>
+					</div>
+					<div>
+						<img src="/static/login/contents/slider_image_2.jpg" alt="slider 2" />
+						<section>
+							<h2>成绩分析</h2>
+							<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							成绩分析为你各个学科的学习情况提供了三个参数值，包括：能力值、稳定指数、进步指数</p>
+						</section>
+						<a href="#" class="imgSliderPrev imgSliderTextPrev"></a>
+						<a href="#" class="imgSliderNext imgSliderTextNext"></a>
+					</div>
+					<div>
+						<img src="/static/login/contents/slider_image_3.jpg" alt="slider 3" />
+						<section>
+							<h2>能力值</h2>
+							<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							能力值表示该学科你的学习成绩在全年级的一个位置，值越大说明该学科你的学习成绩越好</p>
+						</section>
+						<a href="#" class="imgSliderPrev imgSliderTextPrev"></a>
+						<a href="#" class="imgSliderNext imgSliderTextNext"></a>
+					</div>
+					<div>
+						<img src="/static/login/contents/slider_image_4.jpg" alt="slider 4" />
+						<section>
+							<h2>稳定指数</h2>
+							<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							稳定指数表示该学科你的最近学习成绩的稳定性，值越小说明该学科你最近的成绩稳定性越好</p>
+						</section>
+						<a href="#" class="imgSliderPrev imgSliderTextPrev"></a>
+						<a href="#" class="imgSliderNext imgSliderTextNext"></a>
+					</div>
+					<div>
+						<img src="/static/login/contents/slider_image_5.jpg" alt="slider 5" />
+						<section>
+							<h2>进步指数</h2>
+							<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							进步指数表示该学科你的最近学习成绩的起伏，正数表示进步，负数表示退步，值越大说明该学科你最近的成绩进步越大</p>
+						</section>
+						<a href="#" class="imgSliderPrev imgSliderTextPrev"></a>
+						<a href="#" class="imgSliderNext imgSliderTextNext"></a>
+					</div>
+				</div><!--imgSliderC-->
+				
+				<a class="imgSliderPrev" href="#">Prev</a>
+				<a class="imgSliderNext" href="#">Next</a>
+				
+				<div class="imgSliderPaginationContainer clearfix" style="margin-left: -95px;">
+					<div class="imgSliderPaginationContainerLeft"></div>
+					<div class="imgSliderPaginationContainerCenter">
+						<ul class="imgSliderPagination clearfix">
+							<li class="imgSliderCurrent"><a href="#0">1</a></li>
+							<li><a href="#1">2</a></li>
+							<li><a href="#2">3</a></li>
+							<li><a href="#3">4</a></li>
+							<li><a href="#4">5</a></li>
+						</ul>
+					</div>	
+					<div class="imgSliderPaginationContainerRight"></div>
+				</div><!--imgSliderPaginationContainer-->
+				
+			</div><!--imgSlider-->
+		</div><!--imgSliderContainer-->
+		<div class="sliderBottomPattern"></div>
 	<div id="loginWin" class="easyui-window" title="登录" style="width:370px;height:270px;padding:2px;"
-		data-options="left:'60%',top:'100px'" 
-		shadow = "true" minimizable="false" maximizable="false" resizable="false" collapsible="false" closable="false"
+		data-options="left:'54%',top:'170px'" 
+		shadow = "false" minimizable="false" maximizable="false" resizable="false" collapsible="false" closable="false"
    		>
 		<div style="float:left;width:320px;padding-left:30px;">
 		    <form id="LoginForm" method="post" action="#">
