05a90e85f4d13e061f73fe5f79a68786ebe92216
abyzsin <abyzsin@gmail.com>
2014-02-08 22:13:30 +0800

    手机登陆页面雏形，index页面存在问题

---
+++ trunk/yipiao/web/views/m/login.php
@@ -0,0 +1,54 @@
+<?php
+$this->breadcrumbs=array(
+	'M'=>array('m/index'),
+	'Login',
+);?>
+<div>
+	<div style="text-align:center;"><b><font face='微软雅黑' size='6'>高级中学成绩查询分析系统</font></b></div> 
+	<br>
+	<div id="loginWin" title="登录">
+		<div style="float:left;width:320px;padding-left:30px;">
+		    <form id="LoginForm" method="post" action="#">
+		        <div style="padding:1px 0;">
+		            <label for="login">用户名:</label>
+		            <input type="text" name="LoginForm[username]" id="username" style="width:260px;"></input>
+		        </div>
+		        <div style="padding:0px 0;">
+		            <label for="password">密码:</label>
+		            <input type="password" name="LoginForm[password]" id="password" style="width:260px;"></input>
+		        </div>
+		        <input type="checkbox" name="LoginForm[rememberMe]" value ="1">记住我<br>
+	            <span style="padding:0px 0;text-align: left;color: red;" id="showMsg"><?php echo $msg; ?></span>
+		    </form>
+		    <div style="text-align:right;padding:5px 0;">
+				<a class="easyui-linkbutton" iconCls="icon-ok" href="javascript:login();" >登录</a>
+				<a class="easyui-linkbutton" iconCls="icon-cancel" href="javascript:cleardata();" >重置</a>
+		    </div>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+document.onkeydown = function(e){
+    var event = e || window.event;  
+    var code = event.keyCode || event.which || event.charCode;
+    if (code == 13) {
+        login();
+    }
+}
+$(function(){
+    $("input[name='username']").focus();
+});
+function cleardata(){
+    $('#LoginForm').form('clear');
+}
+function login(){
+     if($("input[id='username']").val()=="" || $("input[id='password']").val()==""){
+         $("#showMsg").html("用户名或密码为空，请输入");
+         $("input[id='username']").focus();
+    }else{
+          $("#LoginForm").submit();
+        } 
+}
+</script>
