8841a9ab976bb28a2cbede759d75855dd15ee843
abyzsin <abyzsin@gmail.com>
2014-02-11 00:52:51 +0800

    手机版功能已经实现，细节需调整

--- trunk/yipiao/web/controllers/MController.php
+++ trunk/yipiao/web/controllers/MController.php
@@ -74,26 +74,8 @@ class MController extends Controller
             	'id' => 'g-'.$gradeInfo["gradeid"],
 	            'text'=> $gradeInfo["gradename"], 
 	            'selected'=>false,
-	            'iconCls'=>"",
-	            'children' => array());
+	            'iconCls'=>"");
 
-            // 获取年级下的班级
-            $classList = InfoClass::model()->findAllByAttributes(array('GradeID' => $gradeRecord->GradeID, 'State' => 1));
-            foreach ($classList as $classRecord)
-            {
-                $iconCls = "";
-                $classInfo = array_change_key_case((array)$classRecord->getAttributes(), CASE_LOWER);
-                if( $classInfo["type"] == 1)
-                	$iconCls = "icon-tip";
-                else if($classInfo["type"] == 2)
-                	$iconCls = "icon-sum";
-                $gradejson['children'][] = array(
-                	'id' => $classInfo["classid"],
-		            'text'=> $classInfo["classname"], 
-		            'selected'=>false,
-		            'iconCls'=>$iconCls
-                );
-            }
             $result['data'][] = $gradejson;
         }
 		$result['success'] = true;
@@ -117,10 +99,10 @@ class MController extends Controller
     	$roleid = $sessionInfo['role_id'];
     	$subjectid = $sessionInfo['subject_id'];
     	
-    	$classlid	= isset($_POST['ClassID'])?$_POST['ClassID']:0;
+    	$gradeid	= isset($_POST['GradeID'])?$_POST['GradeID']:0;
 
     	$connection=Yii::app()->db; 
-		$sql="select * from v_class_exam where ClassIDq = ".$classlid." and State = 1 order by ExamTime desc";
+		$sql="select distinct examID,ExamName from v_class_exam where GradeID = ".$gradeid." and State = 1 order by ExamTime desc";
 		$rows=$connection->createCommand ($sql)->query();
 		$isfirst = true;
 		foreach ($rows as $k => $v ){
@@ -128,31 +110,10 @@ class MController extends Controller
 			$examjson = array(
             	'id' => $examInfo["examid"],
 	            'text'=> $examInfo["examname"], 
-	            'selected'=>$isfirst,
-	            'subject' => array());
+	            'selected'=>$isfirst
+	            );
 			$isfirst = false;
-			
-			// 获取考试下的科目
-			$connection=Yii::app()->db; 
-			$sql="select * from v_exam_subject where ExamID = ".$examInfo["examid"]." and refersubjectid = '' and State = 1 order by subjectID";
-			$rows=$connection->createCommand ($sql)->query();
-			foreach ($rows as $k => $v ){
-				$subjectInfo = array_change_key_case($v, CASE_LOWER);
-				
-				if($subjectid == $subjectInfo["subjectid"])
-					$examjson['subject'][] = array(
-	                	'id' => $subjectInfo["subjectid"],
-			            'text'=> $subjectInfo["subjectname"], 
-			            'selected'=>true,
-	                );
-				else if($roleid > 2)
-					$examjson['subject'][] = array(
-	                	'id' => $subjectInfo["subjectid"],
-			            'text'=> $subjectInfo["subjectname"], 
-			            'selected'=>false,
-	                );
-			}
-						
+								
 			$result['data'][] = $examjson;
 		}
 		$result['success'] = true;
@@ -172,47 +133,24 @@ class MController extends Controller
     		return;
     	}
     	
-    	$classid 	= isset($_POST['classid'])?$_POST['classid']:'';
-     	$examid 	= isset($_POST['examid'])?$_POST['examid']:'';   	
-    	$subjectids	= isset($_POST['subjectids'])?$_POST['subjectids']:'';
-    	$name		= isset($_POST['name'])?$_POST['name']:'';
-    
-		if('' == $name)
-		{
-        	$recordList = InfoStudent::model()->findAll("ClassID = :ClassID and State = 1",
-        		array('ClassID'=>$classid));
-		}else
-		{ 
-         	$recordList = InfoStudent::model()->findAll("ClassID = :ClassID and Name like '%".$name."%' and State = 1",
-        		array('ClassID'=>$classid));       		
-		}
-        foreach ($recordList as $record)
-        {
-        	$studentInfo = array_change_key_case((array)$record->getAttributes(), CASE_LOWER);
-            $scorejson = array(
-            	'name' => $studentInfo["name"],
-	            'studyno' => $studentInfo["studyno"],
-            	'uid' => $studentInfo["uid"],
-            	'classid' => $studentInfo["classid"],
-            	'gradeid' => $studentInfo["gradeid"],
-            	'examid' => $examid,
-            	'seqid' => strval($studentInfo["uid"])."-".strval($examid)
-	        );
-            $subjectarr = explode(",",$subjectids);  
-            foreach ($subjectarr as $subjectid)
-            {
-            	$scorejson["s".strval($subjectid)] = "";
-            	$recordScoreList = InfoExamScore::model()->findAll("examid = :ExamID and subjectid = ".$subjectid." and uid = ".$studentInfo["uid"],
-        			array('ExamID'=>$examid));
-        		foreach ($recordScoreList as $recordScore)
-        		{		
-        			$scoreInfo = array_change_key_case((array)$recordScore->getAttributes(), CASE_LOWER);
-        			$scorejson["s".strval($subjectid)] = $scoreInfo["score"];
-        		}
-            }
-            
-            $result['data'][] = array_change_key_case($scorejson, CASE_LOWER);
-        }    
+    	$examid 	= isset($_POST['examid'])?$_POST['examid']:'';   	
+    	$connection=Yii::app()->db;
+    	$sql="SELECT SeqID,Score,SubjectName,ClassRank,GradeRank FROM hsoa.info_exam_score sc inner join hsoa.info_subject su on sc.SubjectId=su.SubjectId where examid = ".$examid." and uid = ".$uid;
+    	$rows=$connection->createCommand ($sql)->query();
+
+    	foreach ($rows as $k => $v ){
+    		$examInfo = array_change_key_case($v, CASE_LOWER);
+    		$examjson = array(
+    				'id'=>$examInfo["seqid"],
+    				'score' => $examInfo["score"],
+    				'subjectname'=> $examInfo["subjectname"],
+    				'classrank'=>$examInfo["classrank"],
+    				'graderank'=>$examInfo["graderank"]				
+    		);
+    	
+    		$result['data'][] = $examjson;
+    	}
+          
 		$result['success'] = true;
         $this->renderText(json_encode($result));
 	}	
--- trunk/yipiao/web/views/m/index.php
+++ trunk/yipiao/web/views/m/index.php
@@ -3,31 +3,56 @@ $this->breadcrumbs=array(
 	'M',
 );?>
 <script type="text/javascript">
-$(function(){
-	//载入初始数据
-	alert('shenme ');
+$(document).ready(function(){
+	//载入初始数据	
 	
+	$.getJSON('<?php echo $this->createUrl('getclass'); ?>', function(json){
+		var a=eval(json.data);
+        $.each(a, function(i,item){
+            //var x = json[i];
+            //alert(item.id)
+            $("#man_sco_clacombotree").append("<option value='" +item.id + "'>" + item.text + "</option>" );  
+        })
+    });
+
 	$.ajax({            
 	    type:"POST",   //post提交方式默认是get
-	    url: '<?php echo $this->createUrl('getclass'); ?>',
+	    url: '<?php echo $this->createUrl('getexam'); ?>',
 	    dataType:"json",    
-	    data:{SchoolID: ypschoolid},  
-	    error:function(err) { 
-			//fun_showMsg('提示','班级数据请求失败('+JSON.stringify(err)+')');
+	    data:{GradeID: 1},  
+	    error:function(err) {      // 
+			fun_showMsg('提示','班级数据请求失败('+JSON.stringify(err)+')');
 	    },
 	    success:function(resp) {
 	    	if(resp.success)
 	        {
-	    		//$('#man_sco_clacombotree').combotree('loadData',resp.data);
+	    		var a=eval(resp.data);
+	            $.each(a, function(i,item){
+	                //alert(item.id)
+	                $("#man_sco_exacombobox").append("<option value='" +item.id + "'>" + item.text + "</option>" );  
+	            })
 	        }else
-	        	//fun_showMsg('提示','获取班级数据错误('+resp.msg+')');
+	        	fun_showMsg('提示','获取班级数据错误('+resp.msg+')');
 	    }            
-	});	
-	
-	alert('shenme ');
-	
+	});
+
+
+	$("#man_sco_clacombotree").change(function()
+			{
+			       //alert("Hello");
+			       //alert($("#selectTest").attr("name"));
+			       //$("a").attr("href","xx.html");
+			       //window.location.href="xx.html";
+			       //alert($("#selectTest").val());
+			       alert($("#man_sco_clacombotree option[@selected]").text());
+			       $("#man_sco_clacombotree").attr("value", "2");
+
+			});
+
 	queryscore = function (){
+
 		//判断是否选择
+		/*
 		if(!$('#man_sco_clacombotree').combotree('isValid'))
 		{
 			fun_showMsg('提示','班级不能为空，请修改!');
@@ -37,167 +62,53 @@ $(function(){
 		{
 			fun_showMsg('提示','考试不能为空，请修改!');
 			return;
-		}			
-		//动态定义表格列
-	    var options = {};
-	    options.columns = eval("[["+
-				"{field: 'uid', title: '用户ID' ,hidden:true},"+
-				"{field: 'classid', title: '班级ID' ,hidden:true},"+
-				"{field: 'gradeid', title: '年级ID' ,hidden:true},"+
-				"{field: 'examid', title: '考试ID' ,hidden:true},"+
-	    		"{field: 'name', title: '姓名', width: 80},"+
-	    		"{field: 'studyno', title: '学号', width: 80}"+
-	    	    "]]"); 
-	    var dataid = $('#man_sco_subcombobox').combobox('getValues');
-	    var datatext = $('#man_sco_subcombobox').combobox('getText').split(',');
-	    for(var i = 0; i < dataid.length ; i++)
-	    {
-	    	options.columns[0].push(
-	    	    {field: 's'+dataid[i] , title: datatext[i] , width: 70,
-					editor:{
-						type:'numberbox',
-						options:{
-				        	min:-1,   
-				            precision:0
-		 				}
-					}
-				}
-			);
-	    }
-	    options.columns[0].push(
-		    {field:'action',title:'操作',width:60,align:'center',
-		        formatter:function(value,row,index){
-		            if (row.editing){
-		                var s = '<a href="#" style="color:red" onclick="saverow('+index+','+row.examid+','+row.uid+','+row.classid+','+row.gradeid+')">保存</a> ';
-		                var c = '<a href="#" style="color:red" onclick="cancelrow('+index+','+row.seqid+')">取消</a>  ';
-		                return s+c;
-		            } else {
-		                var e = '<a href="#" style="color:red" onclick="editrow('+index+','+row.seqid+')">修改</a> ';
-		                return e;
-		            }
-		        }
-	    	}
-	    );
-		$grid_score.datagrid(options);
-		$.messager.progress({
-			title : '提示',
-			text : '数据查询中，请稍后....'
-		}); 
+		}	*/		
+   
 		//查询数据
 		$.ajax({            
 			  type:"POST",   //post提交方式默认是get
 			  url: '<?php echo $this->createUrl('getscore'); ?>',
 			  dataType:"json",
 			 // async:false,    
-			  data:{classid: $('#man_sco_clacombotree').combotree('getValues').join(','),
-				  examid: $('#man_sco_exacombobox').combobox('getValues').join(','),
-				  subjectids: $('#man_sco_subcombobox').combobox('getValues').join(','),
-				  name: $('#man_sco_namebox').val()
-				  }, 
+			  data:{				  examid: $("#man_sco_exacombobox").val()				  }, 
 			  error:function(err) {      // 
-					  $.messager.progress('close');
+					  
 					fun_showMsg('提示','成绩数据请求失败('+JSON.stringify(err)+')');
 			  },
 			  success:function(resp) {
-				  $.messager.progress('close');
-			      if(resp.success)
-			      {
-				    	$grid_score.datagrid('loadData',resp.data);
-			      }else
-			      	fun_showMsg('提示','获取成绩数据错误('+resp.msg+')');
+				  if(resp.success)
+			        {
+				        alert(JSON.stringify(resp));
+			    		var a=eval(resp.data);
+			            $.each(a, function(i,item){
+			                //alert(item.id)
+			                $("#score").append(
+					                "<div>科目</div><div>"+item.subjectname+"</div><div>成绩</div><div>"+item.score+"</div><div>排名</div><div>"+item.classrank+"</div><div>年级</div><div>"+item.graderank+"</div>"
+					                );
+			                //$("#man_sco_exacombobox").append("<option value='" +item.id + "'>" + item.text + "</option>" );  
+			            })
+			        }else
+			        	fun_showMsg('提示','获取班级数据错误('+resp.msg+')');
 			  }            
 		});
 	};
+	
+	
 });
 </script>
-
-<table id="man_score_grid"></table>
 <div id="man_score_tb" style="padding:5px;height:auto">   
 	<div style="margin-bottom:5px">    
-		班级: 
-		<input class="easyui-combotree"   
-            id="man_sco_clacombotree"  
-            style="width:150px;height:26px;"
-            data-options="  
-            		valueField:'id',
-                    textField:'text', 
-                    multiple: false , 
-                    required : true,
-                    panelHeight:'auto',
-                    onLoadSuccess : function(node, data){
-                    	for(var i=0;i < data.length;i++)
-                    	{
-                    		for(var j=0;j < data[i].children.length;j++ )
-                    		{
-                    			if(data[i].children[j].selected)
-                    			{
-                    				$('#man_sco_clacombotree').combotree('setValue', data[i].children[j].id);
-                    				return;
-                    			}
-                    		}
-                    	}
-                    },
-                    onBeforeSelect: function(node) 
-	                {            
-                    	if (!$(this).tree('isLeaf', node.target)) 
-                    	{                
-                        	return false;            
-                        }              		
-                   },
-                   onSelect : function(node)
-                   {
-                   		$.ajax({            
-						    type:'POST',   
-						    url: '<?php echo $this->createUrl('getexam'); ?>',
-						    dataType:'json',    
-						    data:{ClassID: node.id},
-						    error:function(err) {      // 
-								fun_showMsg('提示','考试数据请求失败('+JSON.stringify(err)+')');
-						    },
-						    success:function(resp) {
-						    	if(resp.success)
-						        {
-						    		$('#man_sco_exacombobox').combobox('loadData',resp.data);
-						        }else
-						        	fun_showMsg('提示','获取考试数据错误('+resp.msg+')');
-						    }            
-						});
-                   }
-            ">
-		&nbsp;&nbsp;考试名称: 
-	    <input class="easyui-combobox"   
-            id="man_sco_exacombobox"  
-            style="width:150px;height:26px;"
-            data-options="  
-                    valueField:'id',  
-                    textField:'text',  
-                    editable:false,
-                    required : true,
-                    multiple: false,
-                    panelHeight:'auto',
-                    onSelect : function(node)
-                    {
-                    	$('#man_sco_subcombobox').combobox('clear');
-                    	$('#man_sco_subcombobox').combobox('loadData',node.subject);
-                    },
-                    onLoadSuccess : function()
-                    {
-                   		var data = $('#man_sco_exacombobox').combobox('getData');
-                   		for(var i=0;i< data.length;i++)
-                   		{
-                   			if(data[i].selected)
-                   			{
-                    			$('#man_sco_exacombobox').combobox('select', data[i].id);
-                    			return;
-                    		}
-                    	}
-                    }
-            "> 	
+		学年: 
+		<select id="man_sco_clacombotree"  >
+		</select>
+		考试名称:
+		<select id="man_sco_exacombobox"  >
+		</select>
 	    <a href="#" class="easyui-linkbutton" iconCls="icon-search" onclick="queryscore()">查询</a>
+	    <label id="score"></label>
 	</div>
 </div>
-<form id="form" methd="post" action="">
-<input type="hidden" name="ExamID" id="ExamID" />
+
 </form>
 
 
